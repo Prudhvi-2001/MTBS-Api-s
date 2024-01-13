48f819622cefda07c153e85b24dc82c3082721e5
prudhvi-2001 <kuntrapakamprudhvi@gmail.com>
2024-01-13 20:43:59 +0530

    Encryption library Bcryptjs is implemented

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -5,7 +5,8 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { JwtService } from '@nestjs/jwt';
 import {User} from "./schemas/user.schema"
-// import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
+
 @Injectable()
 export class UsersService {
   constructor(
@@ -20,11 +21,16 @@ export class UsersService {
     if (existingUser) {
       throw new ForbiddenException("User Exist with User name !")
     }
-    // const hashedPassword = bcrypt.hash(user.password, 10);
-    const newUser = new this.userModel(user)
-
-    newUser.save()
+        const saltRounds = 10;
+        const salt = bcrypt.genSaltSync(saltRounds);
+        const hashedPassword = await bcrypt.hash(user.password,salt)
+        const newUser = new this.userModel({
+          ...user,
+          password:hashedPassword
+        })
+        await newUser.save()
     return {
+      user:newUser.username,
       message:"User has been Registered Sucessfully!!",
       status:HttpStatus.CREATED
     }
@@ -33,11 +39,9 @@ export class UsersService {
 //To login the User and it will generate a token
   async login(username:string,password:string):Promise<any>{
       const user = await this.userModel.findOne({username}).exec()
-      if (user?.password !== password || user?.username !== username) {
-        throw new UnauthorizedException("Invalid Credentials",{
-          cause:HttpStatus.NOT_FOUND
-        })
-      }
+      if (!user || !await bcrypt.compare(password, user.password)) {
+        throw new UnauthorizedException('Please check your email and password')
+        }
       const hexId = user._id.toHexString()
       const payload = { sub: hexId, username: user.username };
         return {
